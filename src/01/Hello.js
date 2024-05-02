01f262d8002d56800ca09b25512f4e2a09e8cf2f
sungkyun-ryu <sungkyun.ryu@gmail.com>
2024-05-02 17:56:13 +0900

    beginning React

---
+++ src/01/Hello.js
@@ -0,0 +1,13 @@
+function Hello() {
+    return (
+        <>
+            <p>Hello React!</p>
+            <ul>
+                <li>
+                    React Component
+                </li>
+            </ul>
+        </>
+    );
+}
+export default Hello; 
