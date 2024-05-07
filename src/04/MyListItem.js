1adae6001228b9cf02fddf18a4e53d178ad3b9ac
sungkyun-ryu <sungkyun.ryu@gmail.com>
2024-05-07 17:48:41 +0900

    "probs & design"

--- src/04/MyListItem.js
+++ src/04/MyListItem.js
@@ -1,13 +1,13 @@
 
 export default function MyListItem({ title, imgUrl, content }) {
     return (
-        <div className="bg-gray-200  flex w-full h-full justify-center items-start">
-            <div className="flex flex-col w-1/3 justify-center p-5 ">
+        <div className="bg-gray-200  flex w-full justify-center items-start">
+            <div className="flex flex-col h-full w-1/3 justify-center p-5 ">
                 <img src={imgUrl} alt={title} />
             </div>
-            <div className="flex flex-col w-2/3 justify-between">
+            <div className="flex flex-col h-full w-2/3 justify-between mr-3">
                 <div>
-                    <div className="font-bold">
+                    <div className="font-bold mt-3">
                         {title}
                     </div>
                     <div>
@@ -17,7 +17,7 @@ export default function MyListItem({ title, imgUrl, content }) {
                 <div className="flex justify-end">
                     <span>💚</span>
                     <span className="inline-flex mx-2 font-bold">Like</span>
-                    <span className="font-bold">0</span>
+                    <span className="font-bold mr-3">0</span>
                 </div>
             </div>
         </div>
